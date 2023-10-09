631e662873b4c251137aee8a99c2331cf6aae2ff
mayhim182 <mayankatnitrkl@gmail.com>
2023-10-09 18:11:31 +0530

    skeleton

--- src/app/sharing-screen/sharing-screen.component.ts
+++ src/app/sharing-screen/sharing-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-sharing-screen',
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./sharing-screen.component.css']
 })
 export class SharingScreenComponent {
+  @ViewChild('videoElement') videoElement: ElementRef | undefined;
 
+  onlineUsers: string[] = ['Mayank','Tamop Hasy'];
+
+  addUser(username: string): void {
+    this.onlineUsers.push(username);
+  }
+
+  removeUser(username: string): void {
+    const index = this.onlineUsers.indexOf(username);
+    if (index !== -1) {
+      this.onlineUsers.splice(index, 1);
+    }
+  }
+
+  setVideoStream(stream: MediaStream): void {
+    const video: HTMLVideoElement = this.videoElement?.nativeElement;
+    video.srcObject = stream;
+  }
 }
+
+
